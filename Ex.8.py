386076559daf1c1af9ace94352abb496272439e2
Bershadskiy Renat <73740562+Renatgit@users.noreply.github.com>
2021-11-22 21:35:29 +0200

    Delete Ex.8.py

--- Ex.8.py
+++
@@ -1,5 +0,0 @@
-def describe_pet(pet_name, animal_type="собака"):
-   print(f"\nУ меня есть {animal_type}.")
-   print(f"Мой {animal_type} и его зовут {pet_name.title()}.")
- 
-describe_pet(pet_name="вилли")
