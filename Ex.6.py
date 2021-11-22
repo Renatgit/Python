3a5acfed36bca6e13ce27087bb587fcc44226c12
Bershadskiy Renat <73740562+Renatgit@users.noreply.github.com>
2021-11-22 21:35:27 +0200

    Delete Ex.6.py

--- Ex.6.py
+++
@@ -1,6 +0,0 @@
-def greet_user(username, surname):
-    print(f'Hello, {username.title()} {surname.title()}')
-name=input('Введите имя: ')
-name_user=input('Введите Фамилию: ')
-greet_user(name, name_user)
-
