03ca8e2916513613b46487fca11e6c16cd07b832
Bershadskiy Renat <73740562+Renatgit@users.noreply.github.com>
2021-12-23 18:52:40 +0200

    Lesson26

---
+++ Lesson26/Системы(мат_3).py
@@ -0,0 +1,15 @@
+import math
+
+x=int(input('Введите х: '))
+
+if x>=0 and x<2:
+    y=x*math.sqrt(abs(x-5.4))
+    print(y)
+elif x>=2 and x<8:
+    y=math.atan(x**2)
+    print(y)
+elif x>=8:
+    y=math.log10(abs(x-7.8))
+    print(y)
+else:
+    print('Error')
